4998a04696ccaf96c8181d7c665a9f03abf108b3
Juan David Peña <juandavidp76@hotmail.com>
2023-10-27 10:22:10 -0500

    add suppliers route

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 const clients = require('./api/clients');
 const orders = require('./api/orders');
 const items = require('./api/items');
+const suppliers = require('./api/suppliers');
 
 function routes(app) {
   app.use('/api/clients', clients);
   app.use('/api/orders', orders);
   app.use('/api/items', items);
+  app.use('/api/suppliers', suppliers);
 }
 
 module.exports = routes;
